1efe20542ab91aa00486a184855a684c66b98108
Liam McCurdy <156491352+LMC2605@users.noreply.github.com>
2024-01-16 10:59:13 -0500

    Removed and added print lines
    Removed a print line from script1.py
    Added a print line to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #lines from Zen of Python
 print("If the implementation is easy to explain, it may be a good idea")
 print("Namespaces are one honking great idea -- let's do more of those!")
+print("Unless explicitly silenced.")
